106f754eb29429cc4dd51a9b80a0d7757d9d5e2f
CoxAutoLearns <manheimtraininggroup@gmail.com>
2016-12-20 14:33:00 -0500

    First upload

---
+++ NewHire/mobile/js/info.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+var base_url="../1D30969A6606FC7725B12AEC96EBDBE1/";  
+var  number_of_pages=42;
+var needle=window.location.href.search("/mobile/");
+var mobile_url=(window.location.href.substr(0,needle))+"/mobile/";
+var res_url=(window.location.href).substr(0,needle)+"/"+base_url.substr(3)+"res/";
+var mobile_url=""; 
+var viewer_url=""; 
+function check_settings() {
+		
+	 
+}
